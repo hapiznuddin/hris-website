a9b626834536a3a2d6a37d82d448a594dd73bd47
Hapiz Nuddin Setiadi <55013162+hapiznuddin@users.noreply.github.com>
2025-05-04 20:04:14 +0700

    Add files via upload

---
+++ Employee.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($employee) {
+            if (empty($employee->nip)) {
+                $today = Carbon::now()->format('Ymd');
+                $count = self::whereDate('created_at', Carbon::today())->count() + 1;
+                $employee->nip = $today . str_pad($count, 4, '0', STR_PAD_LEFT);
+            }
+        });
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function leaves()
+    {
+        return $this->hasMany(LeaveRequest::class, 'employee_id');
+    }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class, 'employee_id');
+    }
+}
