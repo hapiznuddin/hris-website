3afced82ba76f4309fbc8a9738b75fc0e99bbf53
Hapiz Nuddin Setiadi <hapiznuddin@gmail.com>
2025-05-16 13:51:29 +0700

    Update EmployeeStats.php

--- app/Filament/Resources/EmployeeResource/Widgets/EmployeeStats.php
+++ app/Filament/Resources/EmployeeResource/Widgets/EmployeeStats.php
@@ -13,9 +13,9 @@ class EmployeeStats extends BaseWidget
     {
         return [
             Stat::make('Total Pegawai', Employee::count())->icon('heroicon-s-users'),
-            Stat::make('Pegawai Tetap', Employee::where('contract', 'tetap')->count())->icon('heroicon-s-users'),
-            Stat::make('Pegawai Kontrak', Employee::where('contract', 'kontrak')->count())->icon('heroicon-s-users'),
-            Stat::make('Pegawai Magang', Employee::where('contract', 'magang')->count())->icon('heroicon-s-users'),
+            Stat::make('Pegawai Tetap', Employee::where('contract', 'Tetap')->count())->icon('heroicon-s-users'),
+            Stat::make('Pegawai Kontrak', Employee::where('contract', 'Kontrak')->count())->icon('heroicon-s-users'),
+            Stat::make('Pegawai Magang', Employee::where('contract', 'Magang')->count())->icon('heroicon-s-users'),
         ];
     }
 }
